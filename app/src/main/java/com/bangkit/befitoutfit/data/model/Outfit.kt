ca92f8a5713ee9bbdd4f1e40c8d786bc8b7ee32e
promecarus <haikalslipi@gmail.com>
2023-12-12 23:56:00 +0700

    🚧 misc: rearrange

--- app/src/main/java/com/bangkit/befitoutfit/data/model/Outfit.kt
+++ app/src/main/java/com/bangkit/befitoutfit/data/model/Outfit.kt
@@ -2,9 +2,9 @@ package com.bangkit.befitoutfit.data.model
 
 data class Outfit(
     val id: Int = 0,
+    val imageUrl: String = "",
     val name: String = "",
     val type: String = "",
     val event: String = "",
-    val imageUrl: String = "",
     val include: Boolean = false,
 )
